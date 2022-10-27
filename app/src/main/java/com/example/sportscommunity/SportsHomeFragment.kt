1beee765cbf12da27a90d07c36bfb2d93d3b18c1
oxen6297 <oxen6297@naver.com>
2022-10-27 09:09:54 +0900

    홈 탭 수정

--- app/src/main/java/com/example/sportscommunity/SportsHomeFragment.kt
+++ app/src/main/java/com/example/sportscommunity/SportsHomeFragment.kt
@@ -5,13 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sportscommunity.databinding.SportsHomeFragmentBinding
 
 class SportsHomeFragment:Fragment() {
+
+    private var mBinding: SportsHomeFragmentBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sports_home_fragment, container, false)
+    ): View {
+        mBinding = SportsHomeFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
