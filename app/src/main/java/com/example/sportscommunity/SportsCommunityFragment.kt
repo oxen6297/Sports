3285a5a21dd10c1bb9ee81ae2f552f817b102005
oxen6297 <oxen6297@naver.com>
2022-10-29 15:21:42 +0900

    커뮤니티 탭 화면 구성

--- app/src/main/java/com/example/sportscommunity/SportsCommunityFragment.kt
+++ app/src/main/java/com/example/sportscommunity/SportsCommunityFragment.kt
@@ -5,13 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sportscommunity.databinding.SportsCommunityFragmentBinding
+import com.example.sportscommunity.databinding.SportsHomeFragmentBinding
 
-class SportsCommunityFragment:Fragment() {
+class SportsCommunityFragment : Fragment() {
+
+    private var mBinding: SportsCommunityFragmentBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sports_community_fragment, container, false)
+    ): View {
+        mBinding = SportsCommunityFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.foldingLayout.setOnClickListener {
+            if (binding.detailLayout.visibility == View.GONE) {
+                binding.detailLayout.visibility = View.VISIBLE
+                binding.expandBtn.animate().apply {
+                    duration = 300
+                    rotation(180f)
+                }
+            } else {
+                binding.detailLayout.visibility = View.GONE
+                binding.expandBtn.animate().apply {
+                    duration = 300
+                    rotation(0f)
+                }
+            }
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
