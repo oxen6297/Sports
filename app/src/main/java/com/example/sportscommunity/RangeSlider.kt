09c6188d90cc9cd7230ec9cb02fcbb6af2a1f7c4
oxen6297 <oxen6297@naver.com>
2022-11-11 11:58:16 +0900

    함께해요 단체 탭(프라그먼트) 바텀 시트 다이얼로그 구현

---
+++ app/src/main/java/com/example/sportscommunity/RangeSlider.kt
@@ -0,0 +1,59 @@
+package com.example.sportscommunity
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sportscommunity.databinding.SortNumberBottomSheetDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.slider.RangeSlider
+
+class RangeSlider : BottomSheetDialogFragment() {
+
+    private var mBinding: SortNumberBottomSheetDialogBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = SortNumberBottomSheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+
+                Log.d("range2", slider.values[0].toString())
+                Log.d("range3", slider.values[1].toString())
+
+                binding.minText.text = "최소 인원: " + slider.values[0].toInt().toString() + "명"
+                binding.maxText.text = "최대 인원: " + slider.values[1].toInt().toString() + "명"
+
+                binding.saveBtn.setOnClickListener {
+
+                }
+            }
+        })
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
+    }
+
+    override fun getTheme(): Int = R.style.BottomSheetDialog
+}
--- app/src/main/java/com/example/sportscommunity/SportsMapGroupFragment.kt
+++ app/src/main/java/com/example/sportscommunity/SportsMapGroupFragment.kt
@@ -1,24 +1,37 @@
 package com.example.sportscommunity
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.LinearLayout
-import androidx.appcompat.app.ActionBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.ViewGroupUtils
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import com.example.sportscommunity.Adapter.backPressed
 import com.example.sportscommunity.databinding.SportsMapGroupFragmentBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.slider.BaseOnSliderTouchListener
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
+import org.w3c.dom.Text
 
 class SportsMapGroupFragment : Fragment() {
 
     //그룹 모집 탭
-
     private var mBinding: SportsMapGroupFragmentBinding? = null
     private val binding get() = mBinding!!
+
+    private var flag = 0
+    private var flags = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +42,112 @@ class SportsMapGroupFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mainActivity = (activity as MainActivity)
+        mainActivity.hideBottomNavigationView(false)
+
+        binding.run {
+
+            groupSortTime.setOnClickListener {
+
+                if (flag == 0) {
+                    groupSortTime.setBackgroundResource(R.drawable.select_background)
+                    groupSortTime.setTextColor(Color.WHITE)
+                    flag = 1
+                } else if (flag == 1) {
+                    groupSortTime.setBackgroundResource(R.drawable.edit_text_background)
+                    groupSortTime.setTextColor(R.color.black)
+                    flag = 0
+                }
+            }
+
+            groupSortArea.setOnClickListener {
+                if (flags == 0) {
+                    groupSortArea.setBackgroundResource(R.drawable.select_background)
+                    groupSortArea.setTextColor(Color.WHITE)
+                    flags = 1
+                } else if (flags == 1) {
+                    groupSortArea.setBackgroundResource(R.drawable.edit_text_background)
+                    groupSortArea.setTextColor(R.color.black)
+                    flags = 0
+                }
+            }
+
+
+            groupSortCategory.setOnClickListener {
+
+                val bottomSheetDialog =
+                    BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+                val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
+                    R.layout.sort_group_bottom_sheet_dialog,
+                    view.findViewById(R.id.group_sort_bottom_sheet) as LinearLayout?,
+                    false
+                )
+
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_ball, groupSortCategory, "구기종목",
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_leisure, groupSortCategory, "레저",
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_water, groupSortCategory, "해양",
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_life, groupSortCategory, "생활",
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_winter, groupSortCategory, "동계",
+                )
+                bottomSheet(
+                    bottomSheetDialog,
+                    bottomSheetView,
+                    R.id.sort_e_sports,
+                    groupSortCategory,
+                    "이스포츠",
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_all, groupSortCategory, "전체",
+                )
+            }
+            val bottomSheetDialogFragment = com.example.sportscommunity.RangeSlider()
+
+            groupSortNumberMember.setOnClickListener {
+
+                bottomSheetDialogFragment.show(childFragmentManager, bottomSheetDialogFragment.tag)
+
+            }
+
+            groupSortManWoman.setOnClickListener {
+
+                val bottomSheetDialog =
+                    BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+                val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
+                    R.layout.sort_man_bottom_sheet_dialog,
+                    view.findViewById(R.id.man_sort_bottom_sheet) as LinearLayout?,
+                    false
+                )
+
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_man, groupSortManWoman, "남자"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_woman, groupSortManWoman, "여자"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_all, groupSortManWoman, "전체",
+                )
+            }
+        }
 
         requireActivity().addMenuProvider(object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
@@ -39,13 +156,13 @@ class SportsMapGroupFragment : Fragment() {
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+
                 // Handle the menu selection
                 return when (menuItem.itemId) {
                     R.id.options_menu -> {
 
-                        val mainActivity = (activity as MainActivity)
-
-                        val bottomSheetDialog = BottomSheetDialog(requireContext())
+                        val bottomSheetDialog =
+                            BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
                         val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
                             R.layout.bottom_sheet_dialog,
                             view.findViewById(R.id.bottom_sheet) as LinearLayout?
@@ -79,7 +196,29 @@ class SportsMapGroupFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        (activity as AppCompatActivity).supportActionBar?.title = "함께해요"
+        (activity as AppCompatActivity).supportActionBar?.title = "함께해요 (단체)"
 
+    }
+
+    private fun bottomSheet(
+        bottomSheetDialog: BottomSheetDialog,
+        view: View,
+        intTree: Int,
+        textView: TextView,
+        text: String
+    ) {
+
+        view.findViewById<View>(intTree).setOnClickListener {
+            textView.text = text
+            textView.setTextColor(Color.WHITE)
+            textView.setBackgroundResource(R.drawable.select_background)
+            bottomSheetDialog.dismiss()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mBinding = null
     }
 }
