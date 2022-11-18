ba3d6867ecfc3b210d16369fd673250c385ebb1c
oxen6297 <oxen6297@naver.com>
2022-11-18 15:13:02 +0900

    뉴스 API 코루틴 사용

--- app/src/main/java/com/example/sportscommunity/SportsHomeFragment.kt
+++ app/src/main/java/com/example/sportscommunity/SportsHomeFragment.kt
@@ -12,6 +12,9 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sportscommunity.Adapter.backPressed
 import com.example.sportscommunity.databinding.SportsHomeFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -38,45 +41,52 @@ class SportsHomeFragment : Fragment() {
         val mainActivity = (activity as MainActivity)
         mainActivity.hideBottomNavigationView(false)
 
-        val retrofitService = Retrofits.getNewsService()
 
+        CoroutineScope(Dispatchers.IO).launch {
+            callNews()
+        }
+
+
+
+    }
+
+
+
+    private fun callNews() {
+        val retrofitService = Retrofits.getNewsService()
         val call: Call<NewsList> = retrofitService.getNewsList()
 
-        binding.run {
+        call.enqueue(object : Callback<NewsList> {
+            override fun onResponse(
+                call: Call<NewsList>,
+                response: Response<NewsList>
+            ) {
+                try {
+                    if (response.isSuccessful) {
 
-            call.enqueue(object : Callback<NewsList> {
-                override fun onResponse(
-                    call: Call<NewsList>,
-                    response: Response<NewsList>
-                ) {
-
-                    try {
-                        if (response.isSuccessful) {
-
-                            newsRecycle.apply {
-                                this.adapter = ListSourceAdapter(
-                                    requireContext(),
-                                    response.body()?.articles
-                                )
-                                this.layoutManager = LinearLayoutManager(
-                                    requireContext(),
-                                    LinearLayoutManager.HORIZONTAL,
-                                    false
-                                )
-                                Log.d("success", "loadSuccess")
-                                Log.d("loadNews", response.body()?.articles.toString())
-                            }
+                        binding.newsRecycle.apply {
+                            this.adapter = ListSourceAdapter(
+                                requireContext(),
+                                response.body()?.articles
+                            )
+                            this.layoutManager = LinearLayoutManager(
+                                requireContext(),
+                                LinearLayoutManager.HORIZONTAL,
+                                false
+                            )
+                            Log.d("success", "loadSuccess")
+                            Log.d("loadNews", response.body()?.articles.toString())
                         }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
                     }
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
+            }
 
-                override fun onFailure(call: Call<NewsList>, t: Throwable) {
-                    Log.d("failed", "failed")
-                }
-            })
-        }
+            override fun onFailure(call: Call<NewsList>, t: Throwable) {
+                Log.d("failed", "failed")
+            }
+        })
     }
 
     fun onClick(v: View?) {
@@ -119,6 +129,4 @@ class SportsHomeFragment : Fragment() {
 
         (activity as AppCompatActivity).supportActionBar?.title = "홈"
     }
-
-
 }
