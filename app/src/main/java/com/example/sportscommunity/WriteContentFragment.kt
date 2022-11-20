1c5994feb39e261ac98bb6efdf2a8dfda35a254a
oxen6297 <oxen6297@naver.com>
2022-11-20 17:53:32 +0900

    탭 버튼 클릭시 프래그먼트 전환 및 버튼 배경 변환

--- app/src/main/java/com/example/sportscommunity/WriteContentFragment.kt
+++ app/src/main/java/com/example/sportscommunity/WriteContentFragment.kt
@@ -1,11 +1,15 @@
 package com.example.sportscommunity
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.sportscommunity.databinding.WriteContentLayoutBinding
 
@@ -13,8 +17,6 @@ class WriteContentFragment : Fragment() {
 
     private var mBinding: WriteContentLayoutBinding? = null
     private val binding get() = mBinding!!
-
-    private lateinit var backPressedCallback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,16 +27,73 @@ class WriteContentFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mainActivity = (activity as MainActivity)
         binding.run {
 
-            saveBtn.setOnClickListener {
-                val title = writeTitle.text.toString()
-                val content = writeContent.text.toString()
+            selectGroupBtn.setOnClickListener {
+                mainActivity.changeWriteFragment(0)
+                setColor(
+                    selectGroupBtn,
+                    selectAloneBtn,
+                    selectCommunityBtn,
+                    R.drawable.login_btn,
+                    Color.WHITE,
+                    R.color.orange,
+                    R.drawable.wirte_content_btn_background
+                )
+            }
+
+            selectAloneBtn.setOnClickListener {
+                mainActivity.changeWriteFragment(1)
+                setColor(
+                    selectAloneBtn,
+                    selectGroupBtn,
+                    selectCommunityBtn,
+                    R.drawable.login_btn,
+                    Color.WHITE,
+                    R.color.orange,
+                    R.drawable.wirte_content_btn_background
+                )
+            }
+
+            selectCommunityBtn.setOnClickListener {
+                mainActivity.changeWriteFragment(2)
+                setColor(
+                    selectCommunityBtn,
+                    selectAloneBtn,
+                    selectGroupBtn,
+                    R.drawable.login_btn,
+                    Color.WHITE,
+                    R.color.orange,
+                    R.drawable.wirte_content_btn_background
+                )
             }
         }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    fun setColor(
+        selectBtn: Button,
+        button: Button,
+        anotherBtn: Button,
+        selectColor: Int,
+        selectTextColor: Int,
+        textColor: Int,
+        color: Int
+    ) {
+
+        selectBtn.setTextColor(selectTextColor)
+        selectBtn.setBackgroundResource(selectColor)
+
+        button.setTextColor(textColor)
+        button.setBackgroundResource(color)
+
+        anotherBtn.setTextColor(textColor)
+        anotherBtn.setBackgroundResource(color)
     }
 
     override fun onAttach(context: Context) {
