6fb836a3d1132989fb7cfff241a8a3ed996c72b5
oxen6297 <oxen6297@naver.com>
2022-11-24 18:32:55 +0900

    string -> string? 에러 수정

--- app/src/main/java/com/example/sportscommunity/Adapter/BindingImageAdapter.kt
+++ app/src/main/java/com/example/sportscommunity/Adapter/BindingImageAdapter.kt
@@ -9,7 +9,7 @@ object BindingImageAdapter{
 
     @BindingAdapter("imageUrl","error")
     @JvmStatic
-    fun loadImage(imageView: ImageView, url: String, error: Drawable) {
+    fun loadImage(imageView: ImageView, url: String?, error: Drawable) {
 
         Glide.with(imageView.context).load(url).fitCenter().error(error).into(imageView)
     }
