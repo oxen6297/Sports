8a3edc94a83bd5edd9c4de2983d7f9d2cdd29a2c
oxen6297 <oxen6297@naver.com>
2022-11-28 13:30:35 +0900

    거래 탭 구성

---
+++ app/src/main/java/com/example/sportscommunity/ShopBottomSheetDialog.kt
@@ -0,0 +1,95 @@
+package com.example.sportscommunity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.sportscommunity.databinding.ShopBottomSheetDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ShopBottomSheetDialog : BottomSheetDialogFragment() {
+
+    private var mBinding: ShopBottomSheetDialogBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = ShopBottomSheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mainActivity = activity as MainActivity
+
+        binding.run {
+            val areaAdapter =
+                ArrayAdapter.createFromResource(
+                    requireContext(),
+                    R.array.areaList,
+                    R.layout.spinner_dropdown_item
+                )
+
+            areaAdapter.setDropDownViewResource(R.layout.spinner_item_style)
+            areaSpinner.adapter = areaAdapter
+
+            areaSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    when(position){
+                        0 -> detailAreaSpinner(R.array.noSelect)
+                        1 -> detailAreaSpinner(R.array.seoulItem)
+                        2 -> detailAreaSpinner(R.array.gyungiList)
+                        3 -> detailAreaSpinner(R.array.incheonList)
+                        4 -> detailAreaSpinner(R.array.daeJeonList)
+                        5 -> detailAreaSpinner(R.array.daeGuList)
+                        6 -> detailAreaSpinner(R.array.ulsanList)
+                        7 -> detailAreaSpinner(R.array.jeonnamList)
+                        8 -> detailAreaSpinner(R.array.jeonbukList)
+                        9 -> detailAreaSpinner(R.array.chungnamList)
+                        10 -> detailAreaSpinner(R.array.chungbukList)
+                        11 -> detailAreaSpinner(R.array.jejuList)
+                        12-> detailAreaSpinner(R.array.sejongList)
+                        13 -> detailAreaSpinner(R.array.gwangjuList)
+                        14 -> detailAreaSpinner(R.array.busanList)
+                        15 -> detailAreaSpinner(R.array.gyungbukList)
+                        16 -> detailAreaSpinner(R.array.gyungnamList)
+                        17 -> detailAreaSpinner(R.array.gangwonList)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    //
+                }
+            }
+
+            saveBtn.setOnClickListener {
+                dialog?.dismiss()
+            }
+        }
+    }
+
+    private fun detailAreaSpinner(item:Int){
+        binding.run {
+            val adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                item,
+                R.layout.spinner_dropdown_item
+            )
+            adapter.setDropDownViewResource(R.layout.spinner_item_style)
+            areaSpinnerTwo.adapter = adapter
+        }
+    }
+
+    override fun getTheme(): Int = R.style.BottomSheetDialog
+}
--- app/src/main/java/com/example/sportscommunity/SportsShopFragment.kt
+++ app/src/main/java/com/example/sportscommunity/SportsShopFragment.kt
@@ -1,28 +1,111 @@
 package com.example.sportscommunity
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.sportscommunity.Adapter.backPressed
+import com.example.sportscommunity.databinding.SportsMapGroupFragmentBinding
+import com.example.sportscommunity.databinding.SportsShopFragmentBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-class SportsShopFragment:Fragment() {
+class SportsShopFragment : Fragment() {
+
+    private var mBinding: SportsShopFragmentBinding? = null
+    private val binding get() = mBinding!!
+    private var flag = 0
+    private var flags = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sports_shop_fragment, container, false)
+    ): View {
+        mBinding = SportsShopFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        arguments?.let {
+            flags = it.getInt("flags")
+        }
+
         val mainActivity = (activity as MainActivity)
         mainActivity.hideBottomNavigationView(false)
+
+        binding.run {
+            shopSortTime.setOnClickListener {
+                if (flag == 0) {
+                    shopSortTime.setBackgroundResource(R.drawable.select_background)
+                    shopSortTime.setTextColor(Color.WHITE)
+                    flag = 1
+
+                } else if (flag == 1) {
+                    shopSortTime.setBackgroundResource(R.drawable.edit_text_background)
+                    shopSortTime.setTextColor(R.color.black)
+                    flag = 0
+                }
+            }
+
+            shopSortCategory.setOnClickListener {
+                val bottomSheetDialog =
+                    BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+                val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
+                    R.layout.sort_group_bottom_sheet_dialog,
+                    view.findViewById(R.id.group_sort_bottom_sheet) as LinearLayout?,
+                    false
+                )
+
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_ball, shopSortCategory, "구기종목"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_leisure, shopSortCategory, "레저"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_water, shopSortCategory, "해양"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_life, shopSortCategory, "생활"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_winter, shopSortCategory, "동계"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_e_sports, shopSortCategory, "이스포츠"
+                )
+                bottomSheet(
+                    bottomSheetDialog, bottomSheetView, R.id.sort_all, shopSortCategory, "전체"
+                )
+            }
+
+            val bottomSheetDialogFragment = ShopBottomSheetDialog()
+
+            shopSortArea.setOnClickListener {
+                bottomSheetDialogFragment.show(childFragmentManager, bottomSheetDialogFragment.tag)
+                if (flags == 0) {
+                    shopSortArea.setBackgroundResource(R.drawable.select_background)
+                    shopSortArea.setTextColor(Color.WHITE)
+
+                } else if (flags == 1) {
+                    shopSortArea.setBackgroundResource(R.drawable.edit_text_background)
+                    shopSortArea.setTextColor(R.color.black)
+                }
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -35,5 +118,21 @@ class SportsShopFragment:Fragment() {
         super.onResume()
 
         (activity as AppCompatActivity).supportActionBar?.title = "중고거래"
+    }
+
+    private fun bottomSheet(
+        bottomSheetDialog: BottomSheetDialog,
+        view: View,
+        intTree: Int,
+        textView: TextView,
+        text: String
+    ) {
+
+        view.findViewById<View>(intTree).setOnClickListener {
+            textView.text = text
+            textView.setTextColor(Color.WHITE)
+            textView.setBackgroundResource(R.drawable.select_background)
+            bottomSheetDialog.dismiss()
+        }
     }
 }
