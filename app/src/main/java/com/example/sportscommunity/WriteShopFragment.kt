89d21c2a0097b3ef31b49341af5e2d7cb43ef720
oxen6297 <oxen6297@naver.com>
2022-12-01 12:13:47 +0900

    거래탭 글 작성 레이아웃 구현

---
+++ app/src/main/java/com/example/sportscommunity/WriteShopFragment.kt
@@ -0,0 +1,152 @@
+package com.example.sportscommunity
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.example.sportscommunity.databinding.WriteShopFragmentBinding
+
+class WriteShopFragment : Fragment() {
+
+    private var mBinding: WriteShopFragmentBinding? = null
+    private val binding get() = mBinding!!
+    lateinit var callback: OnBackPressedCallback
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = WriteShopFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoryAdapter =
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.categoryList,
+                R.layout.spinner_dropdown_item
+            )
+        val areaAdapter =
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.areaList,
+                R.layout.spinner_dropdown_item
+            )
+
+        categoryAdapter.setDropDownViewResource(R.layout.spinner_item_style)
+        areaAdapter.setDropDownViewResource(R.layout.spinner_item_style)
+
+        binding.run {
+            categorySpinner.adapter = categoryAdapter
+            areaSpinner.adapter = areaAdapter
+
+            areaSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    when (position) {
+                        0 -> detailAreaSpinner(R.array.noSelect)
+                        1 -> detailAreaSpinner(R.array.seoulItem)
+                        2 -> detailAreaSpinner(R.array.gyungiList)
+                        3 -> detailAreaSpinner(R.array.incheonList)
+                        4 -> detailAreaSpinner(R.array.daeJeonList)
+                        5 -> detailAreaSpinner(R.array.daeGuList)
+                        6 -> detailAreaSpinner(R.array.ulsanList)
+                        7 -> detailAreaSpinner(R.array.jeonnamList)
+                        8 -> detailAreaSpinner(R.array.jeonbukList)
+                        9 -> detailAreaSpinner(R.array.chungnamList)
+                        10 -> detailAreaSpinner(R.array.chungbukList)
+                        11 -> detailAreaSpinner(R.array.jejuList)
+                        12 -> detailAreaSpinner(R.array.sejongList)
+                        13 -> detailAreaSpinner(R.array.gwangjuList)
+                        14 -> detailAreaSpinner(R.array.busanList)
+                        15 -> detailAreaSpinner(R.array.gyungbukList)
+                        16 -> detailAreaSpinner(R.array.gyungnamList)
+                        17 -> detailAreaSpinner(R.array.gangwonList)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+            saveBtn.setOnClickListener {
+                checkBlank()
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val builder = AlertDialog.Builder(requireContext()).setTitle("맺음")
+                    .setMessage("작성을 취소하시겠습니까?\n확인시 작성중이던 글은 삭제됩니다.")
+                    .setPositiveButton("확인") { dialog, which ->
+
+                        requireActivity().supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container_view, SportsShopFragment())
+                            .commit()
+                    }.setNegativeButton("취소") { dialog, which ->
+                        dialog.dismiss()
+                    }
+                builder.show()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
+    }
+
+    private fun detailAreaSpinner(item: Int) {
+        binding.run {
+            val adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                item,
+                R.layout.spinner_dropdown_item
+            )
+            adapter.setDropDownViewResource(R.layout.spinner_item_style)
+            areaSpinnerTwo.adapter = adapter
+        }
+    }
+
+    private fun checkBlank() {
+        binding.run {
+            if (categorySpinner.selectedItem.toString() == "선택") {
+                Toast.makeText(requireContext(), "카테고리를 입력해주세요", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (areaSpinner.selectedItem.toString() == "시/도") {
+                Toast.makeText(requireContext(), "지역을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if (contentTitleEdit.text.isNullOrBlank()) {
+                Toast.makeText(requireContext(), "제목을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if (contentTextEdit.text.isNullOrBlank()) {
+                Toast.makeText(requireContext(), "내용을 입력해주세요", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (priceEdit.text.isNullOrBlank()) {
+                Toast.makeText(requireContext(), "가격을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("saveBtnClick", "success")
+            }
+        }
+    }
+}
