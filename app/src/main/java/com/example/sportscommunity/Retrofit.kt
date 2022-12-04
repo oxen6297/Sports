30da2a2bbde6f2bb7b87e559b2ffcee239afa81a
oxen6297 <oxen6297@naver.com>
2022-12-04 15:22:38 +0900

    레트로핏, 데이터클래스 생성

--- app/src/main/java/com/example/sportscommunity/Retrofit.kt
+++ app/src/main/java/com/example/sportscommunity/Retrofit.kt
@@ -2,6 +2,7 @@ package com.example.sportscommunity
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -26,30 +27,35 @@ object Retrofits {
     }
 
     fun postUserInfo(): UserInfo {
-        return createRetrofit("http://172.16.100.202:8080/").create(UserInfo::class.java)
+        return createRetrofit("http://192.168.0.38:8080/").create(UserInfo::class.java)
+    }
+
+    fun postCommunity(): WriteCommunity {
+        return createRetrofit("http://192.168.0.38:8080/").create(WriteCommunity::class.java)
+    }
+
+    fun postGroup(): WriteGroup {
+        return createRetrofit("http://192.168.0.38:8080/").create(WriteGroup::class.java)
+    }
+
+    fun postAlone(): WriteAlone {
+        return createRetrofit("http://192.168.0.38:8080/").create(WriteAlone::class.java)
+    }
+
+    fun postShop(): WriteShopInfo {
+        return createRetrofit("http://192.168.0.38:8080/").create(WriteShopInfo::class.java)
+    }
+
+    fun postLike(): LikeInfo {
+        return createRetrofit("http://192.168.0.38:8080/").create(LikeInfo::class.java)
+    }
+
+    fun postChat(): ChatInfo {
+        return createRetrofit("http://192.168.0.38:8080/").create(ChatInfo::class.java)
     }
 }
 
-class User(
-    val id: Int,
-    val email: String?,
-    val gender: String?,
-    val birth: String?,
-    val profileImage: String?,
-    val nickname: String?,
-    val token: String?
-
-) {
-    constructor(
-        email: String?,
-        gender: String?,
-        birth: String?,
-        profileImage: String?,
-        nickname: String?,
-        token: String?
-    ) : this(0, email, gender, birth, profileImage, nickname, token)
-}
-
+// 뉴스 api
 data class News(
     var title: String?,
     var description: String?,
@@ -58,6 +64,22 @@ data class News(
     var publishedAt: String
 )
 
+//회원가입 및 네이버, 카카오 로그인
+class User(
+    val username: String?,
+    val password: String?,
+    val email: String?,
+    val nickname: String?,
+    val mobile: String?,
+    val sns_type: String?,
+    val sns_connect_date: String?,
+    val modify_date: String?,
+    val birth: String?,
+    val image: String?,
+    val gender: String?
+)
+
+//함께해요 단체
 data class GroupPlay(
     var groupImg: String?,
     var groupTitle: String?,
@@ -68,6 +90,24 @@ data class GroupPlay(
     var groupTime: String?
 )
 
+//함께해요 단체 글 작성
+class WriteGroupPlay(
+    var type: String?,
+    var categoryType: String?,
+    var date: String?,
+    var time: String?,
+    var area: String?,
+    var title: String?,
+    var shortContent: String?,
+    var content: String?,
+    var number_member: String?,
+    var gender: String?,
+    var age: String?,
+    var image: String?,
+    var write_time: String?
+)
+
+//함께해요 개인
 data class PlayWith(
     var userImg: String?,
     var playTitle: String?,
@@ -79,6 +119,20 @@ data class PlayWith(
     var manWoman: String?,
     var playComment: String?
 )
+
+//함께해요 개인 글 작성
+class WritePlayWith(
+    var categoryType: String?,
+    var area: String?,
+    var title: String?,
+    var date: String?,
+    var time: String?,
+    var content: String?,
+    var gender: String?,
+    var age: String?,
+    var write_time: String?
+)
+
 
 data class Group(
     var groupImage: String?,
@@ -93,9 +147,16 @@ data class Content(
     var userId: String?,
     var content: String?,
     var like: String?,
-    var bookMark: String?,
     var chat: String?,
-    var share: String?
+)
+
+//커뮤니티 글 작성
+class WriteContent(
+    var categoryType: String?,
+    var title: String?,
+    var content: String?,
+    var image: String?,
+    var write_time: String?
 )
 
 data class Shop(
@@ -112,6 +173,28 @@ data class Shop(
     var price: String?
 )
 
+//중고거래 글 작성
+class WriteShop(
+    var categoryType: String?,
+    var area: String?,
+    var title: String?,
+    var content: String?,
+    var image: String?,
+    var price: String?,
+    var write_time: String?
+)
+
+class Like(
+    var type: String?,
+    var id: String
+)
+
+class Chat(
+    var type: String?,
+    var id: String,
+    var content: String
+)
+
 class NewsList {
     var articles: MutableList<News>? = null
 }
@@ -123,8 +206,50 @@ interface NewsService {
 }
 
 interface UserInfo {
-    @POST("ribbon/.idea/server/apis/sign.php")
+    @POST("ribbon/.idea/server/apis/sign_sns.php")
     fun postUser(
         @Body params: HashMap<String, Any>
     ): Call<User>
+}
+
+interface WriteCommunity {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<WriteContent>
+}
+
+interface WriteGroup {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<WriteGroupPlay>
+}
+
+interface WriteAlone {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<WritePlayWith>
+}
+
+interface WriteShopInfo {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<WriteShop>
+}
+
+interface LikeInfo {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<Like>
+}
+
+interface ChatInfo {
+    @POST("")
+    fun postContent(
+        @Body params: HashMap<String, Any>
+    ): Call<Chat>
 }
