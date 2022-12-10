d86fa015937a18507d6f1d7a1b1fbddedb2dfddd
oxen6297 <oxen6297@naver.com>
2022-12-10 15:21:37 +0900

    커뮤니티 retrofit 함수 구현

--- app/src/main/java/com/example/sportscommunity/MainActivity.kt
+++ app/src/main/java/com/example/sportscommunity/MainActivity.kt
@@ -4,12 +4,20 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Adapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sportscommunity.Adapter.CommunityAdapter
+import com.example.sportscommunity.Adapter.PlayGroupAdapter
 import com.example.sportscommunity.community.*
 import com.example.sportscommunity.databinding.ActivityMainBinding
 import jxl.Sheet
 import jxl.Workbook
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.InputStream
 import java.lang.StringBuilder
 import java.util.*
@@ -156,6 +164,18 @@ class MainActivity : AppCompatActivity() {
                     .setReorderingAllowed(true)
                     .addToBackStack(null).commit()
             }
+            14 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container_view, WriteShopFragment())
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null).commit()
+            }
+            15 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container_view, WriteShopFragment())
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null).commit()
+            }
         }
     }
 
@@ -194,7 +214,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNav.selectedItemId = R.id.map
     }
 
-    fun homeSelected(){
+    fun homeSelected() {
         binding.bottomNav.selectedItemId = R.id.home
     }
 
@@ -206,9 +226,9 @@ class MainActivity : AppCompatActivity() {
         setFragment(fragment)
     }
 
-    fun setDataAtFragmentTwo(fragment: Fragment, title: Int) {
+    fun setDataAtFragmentTwo(fragment: Fragment, title: String, name: String) {
         val bundle = Bundle()
-        bundle.putInt("titles", title)
+        bundle.putString(name, title)
 
         fragment.arguments = bundle
         setFragment(fragment)
@@ -224,5 +244,37 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
         mBinding = null
+    }
+
+    fun callCommunity(recyclerView: RecyclerView, flag: Int) {
+        val retrofitService = Retrofits.getCommunityService()
+        val call: Call<CommunityTab> = retrofitService.getCommunity()
+
+        call.enqueue(object : Callback<CommunityTab> {
+            override fun onResponse(call: Call<CommunityTab>, response: Response<CommunityTab>) {
+                try {
+                    if (response.isSuccessful) {
+                        recyclerView.apply {
+                            this.adapter = CommunityAdapter(
+                                this@MainActivity,
+                                response.body()?.community,
+                                flag
+                            )
+                            this.layoutManager = LinearLayoutManager(
+                                this@MainActivity,
+                                LinearLayoutManager.VERTICAL,
+                                false
+                            )
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(call: Call<CommunityTab>, t: Throwable) {
+                Log.d("failed", "Shop_failed")
+            }
+        })
     }
 }
