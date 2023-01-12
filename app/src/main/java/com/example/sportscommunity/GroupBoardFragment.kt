6f40d80305bec323c61247b3c298a8ff6536073c
oxen6297 <oxen6297@naver.com>
2023-01-12 15:52:33 +0900

    단체 페이지 참여하기 로직 생성

--- app/src/main/java/com/example/sportscommunity/GroupBoardFragment.kt
+++ app/src/main/java/com/example/sportscommunity/GroupBoardFragment.kt
@@ -11,11 +11,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.sportscommunity.databinding.GroupBoardFragmentBinding
+import com.example.sportscommunity.sharedpreference.SharedPreferenceManager
 
 class GroupBoardFragment : Fragment() {
 
     lateinit var binding: GroupBoardFragmentBinding
     lateinit var callback: OnBackPressedCallback
+    private var database: UserDatabase? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +31,42 @@ class GroupBoardFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        database = UserDatabase.getInstance(requireContext())
+
+        val m = "M"
+        val f = "F"
+        val isParticipate =
+            SharedPreferenceManager.getString(requireContext(), "isParticipate", "no")
+
+        if (isParticipate == "yes"){
+            binding.recycleLayout.visibility = View.VISIBLE
+            binding.chatEditLayout.visibility = View.VISIBLE
+        } else {
+            binding.recycleLayout.visibility = View.GONE
+            binding.chatEditLayout.visibility = View.GONE
+        }
 
         val mainActivity = activity as MainActivity
         mainActivity.hideBottomNavigationView(true)
+
+        var userGender = ""
+
+        Thread{
+            database?.userProfileDao()?.getAll()?.forEach {
+                when (it.gender.toString()) {
+                    m -> m.replace("m", "남자")
+                    f -> m.replace("F", "여자")
+                    else -> "null"
+                }.apply {
+                    userGender = this
+                }
+            }
+        }.start()
+
+        val userBirthYear = database?.userProfileDao()?.getAll()?.forEach {
+            it.birthday
+        }
+        val userAge: Int = 2023 - userBirthYear.toString().toInt()
 
         binding.run {
 
@@ -55,14 +90,21 @@ class GroupBoardFragment : Fragment() {
             line.text = lines
             once.text = onces
             gender.text = genders
-            if (minages =="상관없음"){
+            if (minages == "상관없음") {
                 age.text = "상관없음"
-            } else{
+            } else {
                 age.text = minages + "세" + " ~ " + maxages + "세"
             }
             description.text = descriptions
             Glide.with(requireContext()).load(images).fitCenter().error(R.color.orange)
                 .into(groupTitleImg)
+
+            participateBtn.setOnClickListener {
+                if (checkUserInfo(userGender, userAge)) {
+                    recycleLayout.visibility = View.VISIBLE
+                    SharedPreferenceManager.putString(requireContext(), "isParticipate", "yes")
+                }
+            }
         }
     }
 
@@ -81,5 +123,18 @@ class GroupBoardFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun checkUserInfo(gender: String, age: Int): Boolean {
+        val genders = genderHash["gender"].toString()
+        val minages = minageHash["minage"].toString()
+        val maxages = maxageHash["maxage"].toString()
+        return if (gender == genders && minages == "상관없음") {
+            true
+        } else if (minages != "상관없음" && age < maxages.toInt() && minages.toInt() < age) {
+            true
+        } else {
+            false
+        }
     }
 }
